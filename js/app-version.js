10bebdf14c95ef23fdc296c0045bef3f115a9369
LocDaily <329690710+locdaily-store@users.noreply.github.com>
2026-09-19 04:34:24 +0800

    V28.18.2 attendance isolation and permission repair

--- js/app-version.js
+++ js/app-version.js
@@ -1,9 +1,9 @@
 (function(root){
     "use strict";
     const INFO=Object.freeze({
-        appVersion:"27.9.0-v28.18.1",
-        buildVersion:"V28.18.1",
-        buildName:"Product Permission Compatibility Hotfix"
+        appVersion:"27.9.0-v28.18.2",
+        buildVersion:"V28.18.2",
+        buildName:"Attendance Isolation & Permission Repair"
     });
     root.LDM_VERSION_INFO=INFO;
     root.LDM_APP_VERSION=INFO.appVersion;
