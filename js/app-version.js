6ead5184a2b6201af84c875d5a097398e7fe5a32
locdaily-store <329690710+locdaily-store@users.noreply.github.com>
2026-09-21 04:03:46 +0800

    REVISI TEMA TATA LETAK

--- js/app-version.js
+++ js/app-version.js
@@ -1,9 +1,9 @@
 (function(root){
     "use strict";
     const INFO=Object.freeze({
-        appVersion:"27.9.0-v28.19.1-final",
-        buildVersion:"V28.19.1-FINAL",
-        buildName:"Job Role & Account Management Hardening"
+        appVersion:"27.9.0-v28.19.1-ui-fix1",
+        buildVersion:"V28.19.1-UI-FIX1",
+        buildName:"Job Role Dashboard Parity & Public Copy Polish"
     });
     root.LDM_VERSION_INFO=INFO;
     root.LDM_APP_VERSION=INFO.appVersion;
